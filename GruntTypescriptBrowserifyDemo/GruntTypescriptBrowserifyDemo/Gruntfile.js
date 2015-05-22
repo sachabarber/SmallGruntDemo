ebeccdcecd559ef9a9c398886b80deb53b7bd58f
sachabarber <sacha.barber@gmail.com>
2015-05-22 15:18:26 +0100

    1st add

---
+++ GruntTypescriptBrowserifyDemo/GruntTypescriptBrowserifyDemo/Gruntfile.js
@@ -0,0 +1,102 @@
+﻿module.exports = function (grunt) {
+    'use strict';
+
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+
+    //TODO
+    //1. use the grunt-contrib-watch
+
+
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Cleans directories
+        clean: {
+            dist: [
+                'dist/**/*'
+            ],
+            tmp: [
+                'tmp/**/*'
+            ]
+        },
+
+        // Typescript transpiler (Typescript to JS)
+        ts: {
+            options: {
+                fast: 'never',
+                target: 'es5',
+                module: 'commonjs',
+                sourceMap: true
+            },
+            index: {
+                src: ['./src/index.ts'],
+                outDir: './tmp'
+            }
+        },
+
+        // Package 
+        browserify: {
+            './dist/app.js': ['./tmp/*.js', './src/Jquery/jquery.js']
+        },
+
+
+        // Minify 
+        uglify: {
+            //options: {
+            //    mangle: {
+            //        except: ['$', 'require', 'exports'],
+            //    }
+            //},
+            my_target: {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: 'path/to/sourcemap.map'
+                },
+                files: {
+                    './dist/app.js': ['./dist/app.js']
+                }
+            }
+        }
+
+
+    });
+
+    // setup default task to run dev task 
+    grunt.registerTask('default', ['dev']);
+
+    // +++++++++++++++++++++++++++++++++++++++++++
+    // Prod task
+    // +++++++++++++++++++++++++++++++++++++++++++
+    // 1. Cleans directories
+    // 2. Runs typescript transpiling task
+    // 3. Package using Browserify
+    // 4. Minify using Uglify
+    grunt.registerTask('prod', [
+		'clean',
+		'ts:index',
+        'browserify',
+        'uglify'
+    ]);
+
+
+    // +++++++++++++++++++++++++++++++++++++++++++
+    // Dev task
+    // +++++++++++++++++++++++++++++++++++++++++++
+    // 1. Cleans directories
+    // 2. Runs typescript transpiling task
+    // 3. Package using Browserify
+    grunt.registerTask('dev', [
+       'clean',
+       'ts:index',
+       'browserify'
+    ]);
+
+};
---
+++ GruntTypescriptBrowserifyDemo/GruntTypescriptBrowserifyDemo/README.txt
@@ -0,0 +1,30 @@
+﻿1. Get node
+
+
+2. Create empty app shell
+3. open Node command line and change to shell directory location
+4. Install the following packages globally (we may want to use grunt everywhere)
+
+
+npm install -g grunt-cli
+
+
+5. Install the following package
+
+
+npm install grunt --save-dev
+npm install grunt-browserify --save-dev
+npm install grunt-contrib-clean --save-dev
+npm install grunt-ts --save-dev
+npm install jquery --save-dev
+
+
+
+
+
+TODO :
+
+
+Add grunt watch as shown here http://codeofrob.com/entries/grunt+browserify+npm+application=success.html
+
+
